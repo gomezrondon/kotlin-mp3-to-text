d08dfb44ef118c794a337eaaed87e96eddc19a44
gomezrondon <gomezrondon@gmail.com>
2021-08-16 10:28:55 -0600

    Adding another unit test.

--- src/test/kotlin/TestReactiveTest.java
+++ src/test/kotlin/TestReactiveTest.java
@@ -1,13 +1,33 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestReactiveTest {
 
+
+    @Test
+    @DisplayName("testing converting from mono list to flux")
+    public void testingGetListValues(){
+        Mono<List<String>> listMono = Main2Kt.getListValues().collectList();
+        Flux<String> stringFlux = listMono.flatMapMany(Flux::fromIterable);
+
+        stringFlux.subscribe(System.out::println);
+
+
+        StepVerifier.create(stringFlux)
+                .expectNext("a") // count 1
+                .expectNext("javier")// count 2
+                .expectNextCount(1) // count 3
+
+                .verifyComplete();
+    }
 
     @Test
     public void test2(){
